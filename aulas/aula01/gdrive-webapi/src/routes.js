f6c0cc07988011f45a28aa654aba30bc4725521d
Breno Guimaraes <brenoaraujo@outlook.com.br>
2021-12-25 23:45:18 -0300

    feat: set up routes with GET, POST and Options methods

---
+++ aulas/aula01/gdrive-webapi/src/routes.js
@@ -0,0 +1,35 @@
+import { logger } from './logger.js ';
+export class Routes {
+  io;
+  constructor() {}
+
+  setSocketInstace(io) {
+    this.io = io;
+  }
+
+  async defaultRoute(request, response) {
+    response.end('Hello World');
+  }
+
+  async options(request, response) {
+    response.writeHead(204);
+    response.end();
+  }
+
+  async post(request, response) {
+    logger.info('POST - ae');
+    response.end();
+  }
+
+  async get(request, response) {
+    logger.info('GET - ae');
+    response.end();
+  }
+
+  handler(request, response) {
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    const chosen = this[request.method.toLowerCase()] || this.defaultRoute();
+
+    return chosen.apply(this, [request, response]);
+  }
+}
