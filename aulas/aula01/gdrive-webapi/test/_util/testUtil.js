60d457cf264fcaa4013cb30fe4f6295da455013a
Breno Guimaraes <brenoaraujo@outlook.com.br>
2021-12-28 20:00:43 -0300

    feat: add generate writable stream function

--- aulas/aula01/gdrive-webapi/test/_util/testUtil.js
+++ aulas/aula01/gdrive-webapi/test/_util/testUtil.js
@@ -1,14 +1,25 @@
-import { Readable } from 'stream';
+import { Readable, Writable } from 'stream';
 export class TestUtil {
   static generateReadableStream(data) {
     return new Readable({
       objectMode: true,
-      async read() {
+      read() {
         for (const item of data) {
           this.push(item);
         }
 
         this.push(null);
+      },
+    });
+  }
+
+  static generateWritableStream(onData) {
+    return new Writable({
+      objectMode: true,
+      write(chunk, encoding, cb) {
+        onData(chunk);
+
+        cb(null, chunk);
       },
     });
   }
