1643908da282ffbbc725586f979eac6778a699e2
Breno Guimaraes <brenoaraujo@outlook.com.br>
2022-01-05 16:22:45 -0300

    feat: add canExecute function to backpreasure our upload

--- aulas/aula01/gdrive-webapi/src/uploadHandler.js
+++ aulas/aula01/gdrive-webapi/src/uploadHandler.js
@@ -4,23 +4,37 @@ import { pipeline } from 'stream/promises';
 import { logger } from '../src/logger.js';
 
 export class UploadHandler {
-  constructor({ io, socketId, downloadsFolder }) {
+  constructor({ io, socketId, downloadsFolder, messageTimeDelay = 200 }) {
     this.io = io;
     this.socketId = socketId;
     this.downloadsFolder = downloadsFolder;
     this.ON_UPLOAD_EVENT = 'file_upload';
+    this.messageTimeDelay = messageTimeDelay;
+  }
+
+  canExecute(lastExecution) {
+    return Date.now() - lastExecution >= this.messageTimeDelay;
   }
 
   handleFileBytes(filename) {
+    this.lastMessageSent = Date.now();
+
     async function* handleData(source) {
       let alreadyProcessed = 0;
       for await (const chunk of source) {
         yield chunk;
         alreadyProcessed += chunk.length;
 
+        if (!this.canExecute(this.lastMessageSent)) {
+          continue;
+        }
+
+        this.lastMessageSent = Date.now();
+
         this.io
           .to(this.socketId)
           .emit(this.ON_UPLOAD_EVENT, { alreadyProcessed, filename });
+
         logger.info(
           `File [${filename}] got ${alreadyProcessed} bytes to ${this.socketId}`
         );
