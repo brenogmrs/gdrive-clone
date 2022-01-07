70a4b507247661d28984d7878fd836bdde3398c3
Breno Guimaraes <brenoaraujo@outlook.com.br>
2022-01-06 22:45:35 -0300

    test: add integration tests for upload file method

--- aulas/aula01/gdrive-webapi/test/integration/routes.test.js
+++ aulas/aula01/gdrive-webapi/test/integration/routes.test.js
@@ -1,8 +1,39 @@
-import { describe, test, expect, jest } from '@jest/globals';
-import fs from 'fs';
+import {
+  afterAll,
+  beforeAll,
+  beforeEach,
+  describe,
+  jest,
+  test,
+} from '@jest/globals';
 import FormData from 'form-data';
+import fs from 'fs';
+import { tmpdir } from 'os';
+import { join } from 'path';
+import { logger } from '../../src/logger.js';
+import { Routes } from '../../src/routes.js';
 import { TestUtil } from '../_util/testUtil.js';
-describe('routes integration tests', () => [
+describe('routes integration tests', () => {
+  let defaultDownloadFolder = '';
+  beforeAll(async () => {
+    defaultDownloadFolder = await fs.promises.mkdtemp(
+      join(tmpdir(), 'downloads-')
+    );
+  });
+
+  afterAll(async () => {
+    await fs.promises.rm(defaultDownloadFolder, { recursive: true });
+  });
+
+  beforeEach(() => {
+    jest.spyOn(logger, 'info').mockImplementation();
+  });
+
+  const ioObj = {
+    to: (id) => ioObj,
+    emit: (event, message) => {},
+  };
+
   describe('upload file', () => {
     test('should upload file to the folder', async () => {
       const filename = 'mockfile.txt';
@@ -10,7 +41,6 @@ describe('routes integration tests', () => [
         `./test/integration/mocks/${filename}`
       );
       const response = TestUtil.generateWritableStream();
-
       const form = new FormData();
       form.append('textfile', fileStream);
 
@@ -27,6 +57,25 @@ describe('routes integration tests', () => [
         },
         values: () => Object.values(defaultParams),
       };
+
+      const routes = new Routes(defaultDownloadFolder);
+      routes.setSocketInstace(ioObj);
+      const dir = await fs.promises.readdir(defaultDownloadFolder);
+
+      expect(dir).toEqual([]);
+      await routes.handler(...defaultParams.values());
+
+      const dirWithFile = await fs.promises.readdir(defaultDownloadFolder);
+
+      expect(dirWithFile).toEqual([filename]);
+      expect(defaultParams.response.writeHead).toHaveBeenCalledWith(200);
+
+      const expectedResponseEndResult = JSON.stringify({
+        result: 'files uploaded',
+      });
+      expect(defaultParams.response.end).toHaveBeenCalledWith(
+        expectedResponseEndResult
+      );
     });
-  }),
-]);
+  });
+});
